7f4296ab9ac32f44b451412af45233858def8574
Mosaddek <mosaddek123@gmail.com>
2026-01-27 06:37:26 -0500

    Change 2

---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace LearnBlazer.Models
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+
+        public string ProductStatus { get; set; }
+    }
+}
